35a73ef9dfcefbf9ed0f2ad22847e0fa33c41eb6
28vikas <63vikas@gmail.com>
2023-07-15 22:41:18 +0530

    login form

---
+++ Login Form/app.js
@@ -0,0 +1,17 @@
+
+
+
+function ShowPage() {
+ document.querySelector(".form-section").classList.add("show");
+ document.querySelector(".login-btn").classList.add("loginBtn");
+    
+
+}
+
+
+
+function hidepage() {
+   document.querySelector(".form-section").classList.remove("show");
+ document.querySelector(".login-btn").classList.remove("loginBtn");
+
+}
