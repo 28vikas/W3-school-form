eaa6e5340a33ec5c79679fbee6e3daa52aa4887d
28vikas <63vikas@gmail.com>
2023-07-16 22:45:32 +0530

    sign up

---
+++ Sign Up Form/app.js
@@ -0,0 +1,18 @@
+
+
+
+
+function showForm(){
+   document.querySelector(".form-section").classList.add("show");
+   document.querySelector(".Sign-btn").classList.add("btnhide");
+   
+}
+
+
+
+function hideform(){
+   document.querySelector(".form-section").classList.remove("show");
+   document.querySelector(".Sign-btn").classList.remove("btnhide");
+
+
+}
