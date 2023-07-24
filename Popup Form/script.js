10ae59cbfd9ca506fb438b6245b1a19515c62818
28vikas <63vikas@gmail.com>
2023-07-24 19:03:29 +0530

    popup form

---
+++ Popup Form/script.js
@@ -0,0 +1,24 @@
+
+
+let Form = document.getElementById("myForm")
+
+let cls = document.getElementById("cls");
+
+let ops = document.getElementById("ops")
+
+function openForm(){
+    Form.style.display = "block"
+    ops.style.display = "none"
+
+}
+
+
+
+
+
+
+function closeForm(){
+    Form.style.display = "none"
+    ops.style.display = "block"
+    
+}
