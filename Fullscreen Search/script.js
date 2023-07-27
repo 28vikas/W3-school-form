42b84d3824d20164a6eb034db5f983c686a3a09c
28vikas <63vikas@gmail.com>
2023-07-27 17:32:43 +0530

    fullscreen search

---
+++ Fullscreen Search/script.js
@@ -0,0 +1,19 @@
+
+
+
+
+let overlay = document.querySelector(".overlay")
+
+let hideoverlay = document.getElementById("hideoverlay")
+
+
+function openSearch(){
+    hideoverlay.style.display = "block"
+}
+
+
+
+function closeSearch(){
+    hideoverlay.style.display = "none"
+    
+}
